07903b77330cc654a094fd7a41650aeadfda4503
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2023-08-05 18:42:32 +0700

    change: title navbar

--- layout/navigation/navbar.jsx
+++ layout/navigation/navbar.jsx
@@ -18,7 +18,7 @@ export default function Navbar({ children }) {
       path: "/layanan",
     },
     {
-      menu: "Katalog Prodak",
+      menu: "Ayo Jadi Satpam",
       path: "/#katalog",
     },
     {
