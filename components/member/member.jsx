3b33e578a6dc001daddfc063f224764d26b9345b
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2023-08-05 19:12:10 +0700

    add: form for member

--- components/member/member.jsx
+++ components/member/member.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Link from '../general/link'
 
 export default function Member() {
   return (
 	<div id="member" className="mb-10">
 	<h2 className="h2 mb-6">Member JISS</h2>
+	<Link title="Ayo Jadi Member JISS" url="https://forms.gle/SPHiB7kc4etxPEr58"/>
 	</div>
   )
 }
