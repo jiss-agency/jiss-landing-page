82195928439985e3190f3a6ecd1bf5b020100e25
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2023-08-29 22:27:16 +0700

    change member page

--- components/member/member.jsx
+++ components/member/member.jsx
@@ -4,8 +4,8 @@ import Link from '../general/link'
 export default function Member() {
   return (
 	<div id="member" className="mb-10">
-	<h2 className="h2 mb-6">Member JISS</h2>
-	<Link title="Ayo Jadi Member JISS" url="https://forms.gle/SPHiB7kc4etxPEr58"/>
+	<h2 className="h2 mb-6">Join Man Power Jiss</h2>
+	<Link title="Daftar Man Power Jiss" url="https://docs.google.com/forms/d/177x66dwcnQmozuYgDFqWH9tPKqjdyIzE1GfcWx_lxx4/edit?usp=drivesdk"/>
 	</div>
   )
 }
